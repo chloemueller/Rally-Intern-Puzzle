29defdfb4d1aa5cda197cc32c17746fca00e0f64
chloemueller <chloemueller31@gmail.com>
2018-01-12 13:24:56 -0600

    Initial Commit

---
+++ Rally Intern Puzzle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Rally Intern Puzzle
+//
+//  Created by Lisa Ornta on 1/12/18.
+//  Copyright © 2018 Chloe Mueller. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
